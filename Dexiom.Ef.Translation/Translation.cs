19977689a857526117975aa5388e35a7e1daff40
Jonathan Paré <jpare@dexiom.com>
2018-01-21 19:52:08 -0500

    Initial Commit

---
+++ Dexiom.Ef.Translation/Translation.cs
@@ -0,0 +1,9 @@
+﻿namespace Dexiom.Ef.Migration
+{
+    public abstract class Translation<T> where T : Translation<T>, new()
+    {
+        public int Id { get; set; }
+
+        public string CultureName { get; set; }
+    }
+}
---
+++ Dexiom.Ef.Translation/TranslationCollection.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+
+namespace Dexiom.Ef.Migration
+{
+    public class TranslationCollection<T> : Collection<T> where T : Translation<T>, new()
+    {
+        public T this[CultureInfo culture]
+        {
+            get => this[culture.Name];
+            set => this[culture.Name] = value;
+        }
+
+        public T this[string culture]
+        {
+            get
+            {
+                var translation = this.FirstOrDefault(x => x.CultureName == culture);
+                if (translation == null)
+                {
+                    translation = new T {CultureName = culture};
+                    Add(translation);
+                }
+
+                return translation;
+            }
+            set
+            {
+                var translation = this.FirstOrDefault(x => x.CultureName == culture);
+                if (translation != null)
+                {
+                    Remove(translation);
+                }
+
+                value.CultureName = culture;
+                Add(value);
+            }
+        }
+
+        public bool HasCulture(string culture)
+        {
+            return this.Any(x => x.CultureName == culture);
+        }
+
+        public bool HasCulture(CultureInfo culture) => HasCulture(culture.Name);
+
+    }
+}
